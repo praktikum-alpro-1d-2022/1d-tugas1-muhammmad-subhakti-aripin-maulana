038edfe4cdde2b2d870f29a7280399db7dad7a05
subhakti12 <115478042+subhakti12@users.noreply.github.com>
2022-10-10 20:51:07 +0700

    Add files via upload

---
+++ tugas1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){
+   int umur = 18;
+   string npm = "2210631170028";
+   string nama = "M.Subhakti A Maulana";
+   string kelas = "1D";
+   string hobi = "Bermain Gitar";
+   string cita = "Software Enginering";
+   cout << "NPM: " << npm << endl;
+   cout << "Nama Lengkap: " << nama << endl;
+   cout << "Umur: " << umur << endl;
+   cout << "Kelas: " << kelas << endl;
+   cout << "Hobi: " << hobi << endl;
+   cout << "Cita-cita: " << cita << endl;
+
+   return 0;
+}
